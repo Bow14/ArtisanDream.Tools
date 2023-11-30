55541cddf7890fab81dc82926399004191b577be
TkBow <thebow25@gmail.com>
2023-11-29 18:08:43 -0700

    Rested back to default settings

--- RigidBodiesAndColliders/ColliderBehaviour.cs
+++ RigidBodiesAndColliders/ColliderBehaviour.cs
@@ -4,16 +4,27 @@ using UnityEngine.Events;
 public class ColliderBehaviour : MonoBehaviour
 {
     private Collider colliderObj;
-    public UnityEvent startEvent, triggerEnterEvent;
+    public UnityEvent startEvent, triggerEnterEvent, RaiseEndGameEvent;
+    public GameManager gameManagerObj;
     protected virtual void Start()
     {
         colliderObj = GetComponent<Collider>();
         colliderObj.isTrigger = true;
         startEvent.Invoke();
+        gameManagerObj = FindObjectOfType<GameManager>();
+
     }
     
     protected virtual void OnTriggerEnter(Collider other)
     {
         triggerEnterEvent.Invoke();
+        gameManagerObj.LoseLife();
     }
+    
+    public void EndGame()
+    {
+        RaiseEndGameEvent.Invoke();
+    }
+    
+    
 }
